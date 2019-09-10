13628c3d4ccb400f8c03396e123fd54580aaace5
Alejandro Antivero <aantivero@cajval.sba.com.ar>
2019-09-10 09:21:22 -0300

    Utilizando routes y parametros

--- hello-microservice-http/src/main/java/com/aantivero/vertx/http/HelloMicroservice.java
+++ hello-microservice-http/src/main/java/com/aantivero/vertx/http/HelloMicroservice.java
@@ -1,14 +1,19 @@
 package com.aantivero.vertx.http;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.ext.web.Router;
 
 public class HelloMicroservice extends AbstractVerticle {
 
     @Override
     public void start() {
+        Router router = Router.router(vertx);
+        router.get("/").handler(rc -> rc.response().end("Hello Router"));
+        router.get("/:name").handler(rc -> rc.response()
+        .end("Hola " + rc.pathParam("name")));
+
         vertx.createHttpServer()
-                .requestHandler(
-                        req -> req.response().end("Hello"))
+                .requestHandler(router)
                 .listen(8080);
     }
 
