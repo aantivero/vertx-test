70db2e76ccb89a3dbdafdc4b4854ece4a73ccb27
Alejandro Antivero <aantivero@cajval.sba.com.ar>
2019-09-10 09:31:17 -0300

    Generar y devolver un JSON

--- hello-microservice-http/src/main/java/com/aantivero/vertx/http/HelloMicroservice.java
+++ hello-microservice-http/src/main/java/com/aantivero/vertx/http/HelloMicroservice.java
@@ -1,7 +1,10 @@
 package com.aantivero.vertx.http;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.http.HttpHeaders;
+import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
 
 public class HelloMicroservice extends AbstractVerticle {
 
@@ -9,12 +12,22 @@ public class HelloMicroservice extends AbstractVerticle {
     public void start() {
         Router router = Router.router(vertx);
         router.get("/").handler(rc -> rc.response().end("Hello Router"));
-        router.get("/:name").handler(rc -> rc.response()
-        .end("Hola " + rc.pathParam("name")));
+        router.get("/:name").handler(this::hello);
 
         vertx.createHttpServer()
                 .requestHandler(router)
                 .listen(8080);
+    }
+
+    private void hello(RoutingContext routingContext) {
+        String message = "Holis";
+        if (routingContext.pathParam("name") != null) {
+            message += " " + routingContext.pathParam("name");
+        }
+        JsonObject jsonObject = new JsonObject().put("message", message);
+        routingContext.response()
+                .putHeader(HttpHeaders.CONTENT_TYPE, "application/json")
+                .end(jsonObject.encode());
     }
 
 }
